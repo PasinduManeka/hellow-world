faf2c0b99a0bc214b09d893a15b449b9beda1e52
PasinduManeka <manekaherat815@gmail.com>
2021-03-09 04:46:38 +0530

    made student class

---
+++ src/hello/student.java
@@ -0,0 +1,37 @@
+package hello;
+
+public class student {
+	private String sid;
+	private String name;
+	private int age;
+	
+	public String getSid() {
+		return sid;
+	}
+	public void setSid(String sid) {
+		this.sid = sid;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public float getGpa() {
+		return gpa;
+	}
+	public void setGpa(float gpa) {
+		this.gpa = gpa;
+	}
+	private float gpa;
+	
+}
